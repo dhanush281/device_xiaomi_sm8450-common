215b933815838409de02fd136bfd89e0b03218d3
Shaleen Agrawal <shalagra@quicinc.com>
2025-08-04 12:25:02 +0530

    sm8450-common: Configure PELT half-life to 8 at boot
    The default PELT half-life is 32ms. Change it to 8ms.
    
    Change-Id: I20b0f2209ebae45ef544cd2ba11ce13221dbc971
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: rahul <rahul@aospa.co>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -101,7 +101,8 @@ BOARD_KERNEL_CMDLINE := \
     video=vfb:640x400,bpp=32,memsize=3072000 \
     disable_dma32=on \
     mtdoops.fingerprint=$(LINEAGE_VERSION)
-    irqaffinity=0-3
+    irqaffinity=0-3 \
+    pelt=8
 
 BOARD_BOOTCONFIG := \
     androidboot.hardware=qcom \
